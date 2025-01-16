628129553a81b01bf0969a612df6560f58e04dc6
RBing123 <ryan3552215@gmail.com>
2025-01-16 16:28:35 +0800

    test image input and encode

--- src/test/scala/jpeg/jpegTester.scala
+++ src/test/scala/jpeg/jpegTester.scala
@@ -4,7 +4,10 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 import java.beans.beancontext.BeanContextChildSupport
-
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+import java.io.File
+import chisel3.util.log2Ceil
 /**
   * Top level test harness
   */
@@ -15,132 +18,131 @@ class JPEGEncodeChiselTest extends AnyFlatSpec with ChiselScalatestTester {
         * @param data Input pixel data
         * @param encoded Expected encoded output
         */
-    def doJPEGEncodeChiselTest(data: Seq[Seq[Int]], p: JPEGParams): Unit = {
+    def readbmp(filepath: String): Seq[Seq[Int]]={
+        val img = ImageIO.read(new File(filepath))
+        if (img == null) {
+            throw new RuntimeException(s"Failed to read image: $filepath")
+        }
+        val width = img.getWidth
+        val height = img.getHeight
+        require(width % 8 == 0 && height % 8 == 0, 
+                s"Image dimensions must be multiples of 8. Current size: ${width}x${height}")
+        val data = for (y <- 0 until height) yield {
+            for (x <- 0 until width) yield {
+                val pixel = img.getRGB(x, y)
+                val red = (pixel >> 16) & 0xff
+                val green = (pixel >> 8) & 0xff
+                val blue = pixel & 0xff
+                
+                (red * 0.299 + green * 0.587 + blue * 0.114).toInt
+            }
+        }
+        data.toSeq
+    }
+    def splitInto8x8Blocks(data: Seq[Seq[Int]]): Seq[Seq[Seq[Int]]] = {
+        val height = data.length
+        val width = data.head.length
+        
+        val blocks = for {
+            y <- 0 until height by 8
+            x <- 0 until width by 8
+        } yield {
+            for (i <- 0 until 8) yield {
+                for (j <- 0 until 8) yield {
+                    data(y + i)(x + j)
+                }
+            }
+        }
+        blocks.map(_.toSeq).toSeq
+    }
+    def doJPEGEncodeChiselTest(bmpPath: String, p: JPEGParams): Unit = {
+        val data = readbmp(bmpPath)
+        val blocks = splitInto8x8Blocks(data)
+        println(s"Total blocks: ${blocks.length}")
+
         test(new JPEGEncodeChisel(p)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
             dut.clock.setTimeout(0)
-
-            println("Starting Encode")
-            // Testing DCT
-            val jpegEncoder = new jpegEncode(false, List.empty, 0)
-            val expectedDCT = jpegEncoder.DCT(data)
-            val expectedDCTInt: Seq[Seq[Int]] = expectedDCT.map(_.map(_.toInt))
-            val convertedMatrix: Seq[Seq[SInt]] = expectedDCT.map(row => row.map(value => value.toInt.S))
-
-            // Initialize input
-            dut.io.in.valid.poke(true.B)
-            // Set input pixel data
-            for (i <- 0 until p.givenRows) {
-                for (j <- 0 until p.givenCols) {
-                dut.io.in.bits.pixelDataIn(i)(j).poke(data(i)(j).S)
+            var previousDcValue = 0
+            for ((block, blockIndex) <- blocks.zipWithIndex) {
+                println(s"Processing block $blockIndex")
+                println("Starting Encode")
+                
+                // Testing DCT
+                val jpegEncoder = new jpegEncode(false, List.empty, 0)
+                val expectedDCT = jpegEncoder.DCT(block)
+                val expectedDCTInt: Seq[Seq[Int]] = expectedDCT.map(_.map(_.toInt))
+                val convertedMatrix: Seq[Seq[SInt]] = expectedDCT.map(row => row.map(value => value.toInt.S))
+                
+                // Initialize input
+                dut.io.in.valid.poke(true.B)
+                // Set input pixel data
+                for (i <- 0 until 8) {
+                    for (j <- 0 until 8) {
+                        dut.io.in.bits.pixelDataIn(i)(j).poke(block(i)(j).S)
+                    }
                 }
-            }
 
-            // Take step
-            dut.clock.step(3)
-            for (i <- 0 until 8) {
-                for (j <- 0 until 8) {
-                    dut.io.dctOut(i)(j).expect(convertedMatrix(i)(j))
+                // Take step
+                dut.clock.step(3)
+                for (i <- 0 until 8) {
+                    for (j <- 0 until 8) {
+                        dut.io.dctOut(i)(j).expect(convertedMatrix(i)(j))
+                    }
                 }
-            }
-            println("Passed Discrete Cosine Transform")
-            
-            // Testing Quant
-            val expectedQuant = jpegEncoder.scaledQuantization(expectedDCTInt, p.getQuantTable)
-            dut.clock.step()
-            dut.clock.step(64)
-            for (r <- 0 until p.numRows) {
-                for (c <- 0 until p.numCols) {
-                    dut.io.quantOut(r)(c).expect(expectedQuant(r)(c).S)
+                println("Passed Discrete Cosine Transform")
+                
+                // Testing Quant
+                val expectedQuant = jpegEncoder.scaledQuantization(expectedDCTInt, p.getQuantTable)
+                dut.clock.step()
+                dut.clock.step(64)
+                for (r <- 0 until p.numRows) {
+                    for (c <- 0 until p.numCols) {
+                        dut.io.quantOut(r)(c).expect(expectedQuant(r)(c).S)
+                    }
                 }
-            }
-            println("Passed Quantization")
+                println("Passed Quantization")
 
-            // Testing Zigzag
-            val expectedZigzag = jpegEncoder.zigzagParse(expectedQuant)
-            dut.clock.step()
-            dut.clock.step(p.totalElements)
-
-            for(i <- 0 until expectedZigzag.length){
-                dut.io.zigzagOut(i).expect(expectedZigzag(i).S)
-            }
-            println("Passed Zigzag")
-
-            // Testing Encode
-            if(p.encodingChoice){
-                val expectedEncode = jpegEncoder.RLE(expectedZigzag)
+                // Testing Zigzag
+                val expectedZigzag = jpegEncoder.zigzagParse(expectedQuant)
                 dut.clock.step()
                 dut.clock.step(p.totalElements)
 
-                // Check the output
-                for (i <- 0 until expectedEncode.length) {
-                    dut.io.encodedRLE(i).expect(expectedEncode(i).S)
+                for(i <- 0 until expectedZigzag.length){
+                    dut.io.zigzagOut(i).expect(expectedZigzag(i).S)
                 }
-                println("Passed Run Length Encoding")
-            }
-            else{
-                val expectedEncode = jpegEncoder.delta(expectedZigzag)
-                dut.clock.step()
-                dut.clock.step(p.totalElements)
+                println("Passed Zigzag")
 
-                // Check the output
-                for (i <- 0 until p.totalElements) {
-                    dut.io.encodedDelta(i).expect(expectedEncode(i).S)
-                }
-                println("Passed Delta Encoding")
+                // Testing Encode
+                // val expectedDcDiff = expectedZigzag(0) - previousDcValue
+                // dut.io.huffmanEncodedDC.bits.expect( /* 預期的 DC Huffman 位元數據 */ )
+                // dut.io.huffmanEncodedDC.length.expect( /* 預期的 DC 編碼長度 */ )
+                // previousDcValue = expectedZigzag(0)
+                // println("Passed DC Huffman Encoding")
+
+                // // RLE 和 AC Huffman 編碼
+                // val expectedRLE = jpegEncoder.RLE(expectedZigzag.tail)
+                // for (i <- 0 until expectedRLE.length by 2) {
+                //     val run = expectedRLE(i)
+                //     val value = expectedRLE(i + 1)
+                //     dut.io.huffmanEncodedAC.bits.expect( /* 預期的 AC Huffman 位元數據 */ )
+                //     dut.io.huffmanEncodedAC.length.expect( /* 預期的 AC 編碼長度 */ )
+                // }
+                // println("Passed AC Huffman Encoding")
             }
-            println("Completed Encoding\n")
         }
     }
 
-    
     behavior of "Top-level JPEG Encode Chisel"
-    it should "Encodes using RLE - IN1 - QT1" in {
+
+    it should "Encode BMP using RLE - QT1" in {
         val p = JPEGParams(8, 8, 1, true)
-        val inputData = DCTData.in1 
-        doJPEGEncodeChiselTest(inputData, p)
+        val bmpPath = "output.bmp"
+        doJPEGEncodeChiselTest(bmpPath, p)
     }
 
-    it should "Encodes using Delta Encoding - IN1 - QT1" in {
+    it should "Encode BMP using Delta Encoding - QT1" in {
         val p = JPEGParams(8, 8, 1, false)
-        val inputData = DCTData.in1 
-        doJPEGEncodeChiselTest(inputData, p)
+        val bmpPath = "output.bmp"
+        doJPEGEncodeChiselTest(bmpPath, p)
     }
-
-    // OPTIONAL WORKING TESTS
-    // REMOVED SINCE IT TAKES 20 MINS TO COMPLETE
-    // it should "Encodes using RLE - IN1 - QT2" in {
-    //     val p = JPEGParams(8, 8, 2, true)
-    //     val inputData = DCTData.in1 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
-
-    // it should "Encodes using Delta Encoding - IN1 - QT2" in {
-    //     val p = JPEGParams(8, 8, 2, false)
-    //     val inputData = DCTData.in1 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
-
-    // it should "Encodes using RLE - IN2 - QT1" in {
-    //     val p = JPEGParams(8, 8, 1, false)
-    //     val inputData = DCTData.in2 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
-
-    // it should "Encodes using RLE - IN2 - QT2" in {
-    //     val p = JPEGParams(8, 8, 2, false)
-    //     val inputData = DCTData.in2 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
-
-    // it should "Encodes using Delta Encoding - IN2 - QT1" in {
-    //     val p = JPEGParams(8, 8, 1, false)
-    //     val inputData = DCTData.in2 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
-
-    // it should "Encodes using Delta Encoding - IN2 - QT2" in {
-    //     val p = JPEGParams(8, 8, 2, false)
-    //     val inputData = DCTData.in2 
-    //     doJPEGEncodeChiselTest(inputData, p)
-    // }
 }
